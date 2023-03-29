267f3bc870e82bb4ac415761350ccc9c1777261e
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-29 14:11:57 -0500

    add attributes into migration files

--- db/migrate/20230329184328_create_movies.rb
+++ db/migrate/20230329184328_create_movies.rb
@@ -1,7 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
-
+      t.string :title
+      t.string :image
+      t.integer :price
       t.timestamps
     end
   end
