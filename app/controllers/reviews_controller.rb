fe0525d19e6bf0bc444c1c3b9854b9ab6d151ab6
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-29 18:09:50 -0500

    added create action in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
-# GET /reviews
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
     reviews = Review.all
     render json: reviews
@@ -8,7 +9,25 @@ class ReviewsController < ApplicationController
   def show
     review = Review.find(params[:id])
     render json: review
-end
+  end
+
+  def create
+    review = Review.create(review_params)
+    if review.errors.empty?
+        render json: review, status: :created, include: [:movie]
+    else
+        render json: review.errors, status: :unprocessable_entity
+    end
+  end
 
 
+
+  private
+  def review_params
+    params.require(:review).permit(:user_name, :score, :comment, :likes, :movie_id, :user_id)
+  end
+
+  def render_not_found
+      render json: { error: "Camper not found" }, status: :not_found
+  end
 end
