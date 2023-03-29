18cc597a0b90c3d1eb88415a45ed05094a0cb4c3
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-29 14:15:17 -0500

    add description attribute into review's migration

--- db/migrate/20230329184416_create_reviews.rb
+++ db/migrate/20230329184416_create_reviews.rb
@@ -4,6 +4,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.string :user_name
       t.integer :score
       t.string :comment
+      t.text :description
       t.integer :movie_id
       t.integer :user_id
       t.timestamps
