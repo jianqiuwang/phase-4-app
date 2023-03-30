aa8841b6ce687dadcdd2385610f7e0cabfba2d6e
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-30 15:10:54 -0500

    added routes for user login logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   resources :reviews
   resources :movies, only: [:index, :show]
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
