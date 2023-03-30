25be570cb79094ebfaf88b84c31cade2095f6e68
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-30 14:47:20 -0500

    generated a new schema

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2023_03_29_184522) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
