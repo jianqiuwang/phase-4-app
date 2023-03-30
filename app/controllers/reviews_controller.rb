5c7a0ebd10c52aa4d89bf1e07c86f84d7e374147
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-29 19:01:35 -0500

    completed the review full CRUD actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
     reviews = Review.all
     render json: reviews
@@ -14,29 +14,34 @@ class ReviewsController < ApplicationController
   def create
     review = Review.create(review_params)
     if review.errors.empty?
-        render json: review, status: :created, include: [:movie]
+      render json: review, status: :created, include: [:movie]
     else
-        render json: review.errors, status: :unprocessable_entity
-    end
-    
-    def update
-        review = Review.find(params[:id])
-        if review.update(review_params)
-          render json: review, status: :ok, include: [:movie]
-        else
-          render json: review.errors, status: :unprocessable_entity
-        end
+      render json: review.errors, status: :unprocessable_entity
     end
   end
 
+  def update
+    review = Review.find(params[:id])
+    if review.update(review_params)
+      render json: review, status: :ok, include: [:movie]
+    else
+      render json: review.errors, status: :unprocessable_entity
+    end
+  end
 
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    head :no_content
+  end
 
   private
+
   def review_params
     params.require(:review).permit(:user_name, :score, :comment, :likes, :movie_id, :user_id)
   end
 
   def render_not_found
-      render json: { error: "Camper not found" }, status: :not_found
+    render json: { error: "Review not found" }, status: :not_found
   end
 end
