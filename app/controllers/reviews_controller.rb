8ebf6f9b0da59d0f470a6b5d817885aa2c516613
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-30 14:22:33 -0500

    added create and show actions in users controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,5 @@
 class ReviewsController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
     reviews = Review.all
     render json: reviews
@@ -11,37 +10,38 @@ class ReviewsController < ApplicationController
     render json: review
   end
 
-  def create
-    review = Review.create(review_params)
-    if review.errors.empty?
-      render json: review, status: :created, include: [:movie]
-    else
-      render json: review.errors, status: :unprocessable_entity
+   def create
+      review = Review.create(review_params)
+      if review.errors.empty?
+        render json: review, status: :created, include: [:movie]
+      else
+        render json: review.errors, status: :unprocessable_entity
+      end
     end
-  end
-
-  def update
-    review = Review.find(params[:id])
-    if review.update(review_params)
-      render json: review, status: :ok, include: [:movie]
-    else
-      render json: review.errors, status: :unprocessable_entity
+    
+    def update
+        review = Review.find(params[:id])
+        if review.update(review_params)
+          render json: review, status: :ok, include: [:movie]
+        else
+          render json: review.errors, status: :unprocessable_entity
+        end
     end
-  end
 
-  def destroy
-    review = Review.find(params[:id])
-    review.destroy
-    head :no_content
-  end
+    def destroy
+        review = Review.find(params[:id])
+        review.destroy
+        head :no_content
+    end
+
+
 
   private
-
   def review_params
     params.require(:review).permit(:user_name, :score, :comment, :likes, :movie_id, :user_id)
   end
 
   def render_not_found
-    render json: { error: "Review not found" }, status: :not_found
+      render json: { error: "Review not found" }, status: :not_found
   end
 end
