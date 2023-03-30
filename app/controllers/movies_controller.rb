971857255231863c62575bde78d51d384358d5f9
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-29 19:03:06 -0500

    added index and show action in movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,11 @@
 class MoviesController < ApplicationController
+    def index
+        movies = Movie.all
+        render json: movies, include: :reviews
+      end
     
+      def show
+        movie = Movie.find(params[:id])
+        render json: movie, include: :reviews
+      end
 end
