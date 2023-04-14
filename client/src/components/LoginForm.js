3f2e68fdbe53052af9a35661e8c16ebeda1e2c4b
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-04-14 15:41:54 -0500

    remove error handling from LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,39 +2,34 @@ import React, { useState } from 'react';
 import './LoginForm.css';
 import { useHistory } from 'react-router-dom';
 
- function LoginForm ({ onLogin }) {
+function LoginForm({ onLogin }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
-  
 
   const handleSubmit = (event) => {
     event.preventDefault();
     fetch("http://localhost:3000/login", {
-        method: "POST",
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username: username, password: password }),
+      method: "POST",
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
       })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Invalid credentials");
-          }
-        })
-        .then((user) => {
+      .then((user) => {
+        if (user) {
           // Call the onLogin function passed as a prop from App.js
           onLogin(user);
           // Redirect to the main page
           history.push("/");
-        })
-        .catch((error) => {
-          console.error("Error during login:", error);
-          // Handle the error, e.g., show an error message
-        });
+        }
+      });
   };
 
   const handleSignUpClick = () => {
@@ -43,31 +38,32 @@ import { useHistory } from 'react-router-dom';
 
   return (
     <div className="login-container">
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit} className="login-form">
-      <div className="input-group">
-        <label>User Name:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="input-group">
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit" className="login-button">Login</button>
-    </form>
-    <button onClick={handleSignUpClick} className="signup-button">
-      Don't have an account? Sign up
-    </button>
-  </div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="input-group">
+          <label>User Name:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="login-button">Login</button>
+      </form>
+      <button onClick={handleSignUpClick} className="signup-button">
+        Don't have an account? Sign up
+      </button>
+    </div>
   );
 };
 
 export default LoginForm;
+
